13898ac0303e755beb885eed549c312ef1ae00da
EstebanOlmedo <esteban.olmedo.ramirez@gmail.com>
2021-01-01 02:08:34 -0600

    Correcciones en el script para cargar archivo csv

--- menu/cargar.php
+++ menu/cargar.php
@@ -1,7 +1,36 @@
 <?php
 if(isset($_FILES['file_name'])){  
-    if(move_uploaded_file($_FILES['file_name']['tmp_name'], 'contents/' . $_FILES['file_name']['name'])){
-        echo "Se cargo con exito\n";
+    $directory = "contents/";
+    $target_file = $directory . $_FILES['file_name']['name'];
+    $file_type = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    $numero_de_fila = -1;
+    if($file_type == "csv"){
+        if(move_uploaded_file($_FILES['file_name']['tmp_name'], $target_file)){
+            if(($gestor = fopen($target_file,"r")) !== FALSE){
+                $pila = array();
+                while(($fila = fgetcsv($gestor)) !== FALSE){
+                    $numero_de_fila++;
+                    if($numero_de_fila === 0){
+                        continue;
+                    }
+                    $pila[$numero_de_fila-1] = $fila;
+                }
+                echo json_encode($pila);
+                fclose($gestor);
+            }
+            else{
+                exit("Hubo un error al tratar de abrir el archivo copiado\n");
+            }
+            if(!unlink($target_file)){
+                exit("Hubo un error al tratar de eliminar el archivo copiado\n");
+            }
+        }
+        else{
+            exit("Error al copiar el archivo subido\n");
+        }
+    }
+    else{ 
+        echo "Se debe subir un archivo csv\n";
     }
 }
 ?>
