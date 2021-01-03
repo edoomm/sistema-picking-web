84f1697e6c968174b61b4a50d19fd6b2eee3cf9d
EstebanOlmedo <esteban.olmedo.ramire@gmail.com>
2021-01-03 07:19:18 -0600

    Correción en la lógica para subir archivo y correcciones en página de apartados y productos

---
+++ php/cargarApartado.php
@@ -0,0 +1,27 @@
+<?php
+include 'db.php';
+include 'leerCSV.php'
+if(isset($_FILES['file_name'])){
+    /*
+        0  -> id_almacen
+        1  -> sku
+        2  -> id_linea
+        3  -> apartado
+    */
+    $datos = obtener_contenido();
+    //$conn = open_database();
+    foreach($datos as $fila){
+        $id_almacen = $fila[0];
+        $sku = $fila[1];
+        $id_linea = $fila[2];
+        $apartado = $fila[3];
+        $sql_query = "";
+        echo "id_almacen: " . $id_almacen . " SKU: " . $sku . " id_linea " . $id_linea . " apartado: " . $apartado . "\n";
+        //mysqli_query($conn, $sql_query);
+    }
+    //mysqli_close($conn);
+}
+else{
+    echo "El archivo no pudo ser cargado con éxito\n";
+}
+?>
--- php/leerCSV.php
+++ php/leerCSV.php
@@ -1,24 +1,20 @@
 <?php
-include '../php/db.php';
-if(isset($_FILES['file_name'])){  
+function obtener_contenido(){
     $directory = "contents/";
     $target_file = $directory . $_FILES['file_name']['name'];
     $file_type = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
     $numero_de_fila = -1;
+    $datos = array();
     if($file_type == "csv"){
         if(move_uploaded_file($_FILES['file_name']['tmp_name'], $target_file)){
             if(($gestor = fopen($target_file,"r")) !== FALSE){
-                $conn = open_database();
                 while(($fila = fgetcsv($gestor)) !== FALSE){
                     $numero_de_fila++;
                     if($numero_de_fila === 0){
                         continue;
                     }
-                    $sku = $fila[1];
-                    $id_linea = $fila[2];
-                    mysqli_query($conn, $sql_query);
+                    array[$numero_de_fila - 1] = $fila;
                 }
-                mysqli_close($conn);
                 fclose($gestor);
             }
             else{
@@ -35,5 +31,6 @@ if(isset($_FILES['file_name'])){
     else{ 
         echo "Se debe subir un archivo csv\n";
     }
+    return $datos;
 }
 ?>
