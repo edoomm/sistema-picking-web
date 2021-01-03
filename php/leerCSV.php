b39a1d9d267df20c90c533693f616bdc39d6db1c
Esteban Raymundo Olmedo Ramírez <43709368+EstebanOlmedo@users.noreply.github.com>
2021-01-03 14:34:48 -0600

    Modificaciones en el módulo de Productos y de Apartados (#25)
    * Se agregó el popup para subir el archivo CSV
    
    * Se añadió funcionalidad para mostrar los datos de operador una vez que se da click sobre una fila de la tabla
    
    * Script para poder subir el archivo .csv
    
    * Correcciones en el script para cargar archivo csv
    
    * Se corrigió el script para subir archivos csv en el apartado de productos
    
    * Correción en la lógica para subir archivo y correcciones en página de apartados y productos
    
    * Correcciones en la página de operadores y la página de productos
    
    Co-authored-by: EstebanOlmedo <esteban.olmedo.ramire@gmail.com>

---
+++ php/leerCSV.php
@@ -0,0 +1,36 @@
+<?php
+function obtener_contenido(){
+    $directory = "../menu/contents/";
+    $target_file = $directory . $_FILES['file_name']['name'];
+    $file_type = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    $numero_de_fila = -1;
+    $datos = array();
+    if($file_type == "csv"){
+        if(move_uploaded_file($_FILES['file_name']['tmp_name'], $target_file)){
+            if(($gestor = fopen($target_file,"r")) !== FALSE){
+                while(($fila = fgetcsv($gestor)) !== FALSE){
+                    $numero_de_fila++;
+                    if($numero_de_fila === 0){
+                        continue;
+                    }
+                    $datos[$numero_de_fila - 1] = $fila;
+                }
+                fclose($gestor);
+            }
+            else{
+                exit("Hubo un error al tratar de abrir el archivo copiado\n");
+            }
+            if(!unlink($target_file)){
+                exit("Hubo un error al tratar de eliminar el archivo copiado\n");
+            }
+        }
+        else{
+            exit("Error al copiar el archivo subido\n");
+        }
+    }
+    else{ 
+        echo "Se debe subir un archivo csv\n";
+    }
+    return $datos;
+}
+?>
