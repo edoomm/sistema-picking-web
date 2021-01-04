27e75feba11fdeb855e8c11b8e1672fafb798896
Esteban Olmedo <43709368+EstebanOlmedo@users.noreply.github.com>
2021-01-04 07:07:06 -0600

    Revert "Se agregó el script para cargar de manera automática los campos de la base de datos (#27)"
    This reverts commit eb329518efbc929d6641b602457a1ff2658226c1.

--- js/initFile.js
+++ js/initFile.js
@@ -1,37 +1,30 @@
-/*
-    camposFormulario : un arreglo de strings con el id de los elementos del formulario
-    tableId : id de la tabla de elementos
-    tableBD : nombre de la tabla en la BD de la que se cargan los archivos
-    tableBodyId : id del body en donde se van a poner los elementos de la base de datos
-*/
-
-async function init(camposFormulario, tableBD, tableBodyId){
-    await load_table(tableBD, tableBodyId);
-    for(var i = 0; i < camposFormulario.length; i++){
-        document.getElementById(camposFormulario[i]).value="";
-        document.getElementById(camposFormulario[i]).disabled=true;
+function init(){
+    let args = Array.from(arguments);
+    for(var i = 0; i < args.length - 1; i++){
+        document.getElementById(args[i]).value="";
+        document.getElementById(args[i]).disabled=true;
     }
-    rowHandlers(camposFormulario, tableBodyId);
+    rowHandlers(args);
 }
-function aux(){
-    console.log(this);
-}
-function rowHandlers(camposFormulario, tableBodyId){
-    let table = document.getElementById(tableBodyId);
-    console.log(tableBodyId);
-    console.log(table.innerHTML);
+function rowHandlers(args){
+    let table = document.getElementById(args[args.length-1]);
     let rows = table.getElementsByTagName("tr");
-    console.log(rows.length);
-    for(var index = 0; index < rows.length; index++){
-        rows[index].addEventListener("click", function(){
-            let cells = this.getElementsByTagName("td");
-            console.log(cells);
-            let fila = [];
-            for(let j = 0; j < cells.length; j++){
-                fila.push(cells[j].innerHTML);
-            }
-            llenarFormulario(camposFormulario, fila);
-        });
+    args.pop();
+    for(let i = 0; i < rows.length; i++){
+        let currentRow = table.rows[i];
+        let clickHandler = function(nombres,row){
+            return function(){
+                let cells = row.getElementsByTagName("td");
+                let fila = [];
+                for(var j = 0; j < cells.length; j++){
+                    fila.push(cells[j].innerHTML);
+                }
+                llenarFormulario(nombres,fila);
+            };
+        };
+        if(i != 0){
+            currentRow.onclick = clickHandler(args,currentRow);
+        }
     }
 }
 function llenarFormulario(nombres, row){
@@ -43,19 +36,5 @@ function llenarFormulario(nombres, row){
             document.getElementById(nombres[i]).value = row[i];
         }
         document.getElementById(nombres[i]).disabled = true;
-    }
-}
-async function load_table(table_db, table_name){
-    if($(table_db).val() != 0){
-        await $.post("../php/mostrarElementosBD.php", {
-                variable: table_db
-            },
-            function(data){
-                if(data != ""){
-                    console.log(table_name);
-                    document.getElementById(table_name).innerHTML = data;
-                }
-            }
-        );
     }
 }
