08be11acc8ee2f4c427d08c2510ed3955258ba01
edoomm <edoomm8@gmail.com>
2021-01-04 14:53:16 -0600

    correciones para carga masiva (csv)

--- php/db.php
+++ php/db.php
@@ -2,8 +2,8 @@
 
 function open_database() {
     $host = "localhost";
-    $user = "phpmyadmin";
-    $pwd = "Yunue080899";
+    $user = "root";
+    $pwd = "1234";
     $db = "pickingdb";
 
     $conn = mysqli_connect($host, $user, $pwd, $db);
