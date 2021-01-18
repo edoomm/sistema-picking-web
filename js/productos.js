7ae3c0cf570679096f72f6927a3ab3d5e42fce18
Gabriel Graciano Herrera <gabrielgh@live.com.mx>
2021-01-18 16:54:31 -0600

    Se agrego la validacion en el formulario de productos (#47)
    * Se agregó el popup para subir el archivo CSV
    
    * Se añadió funcionalidad para mostrar los datos de operador una vez que se da click sobre una fila de la tabla
    
    * Script para poder subir el archivo .csv
    
    * Correcciones en el script para cargar archivo csv
    
    * Se corrigió el script para subir archivos csv en el apartado de productos
    
    * Correción en la lógica para subir archivo y correcciones en página de apartados y productos
    
    * Correcciones en la página de operadores y la página de productos
    
    * Se agregó el script para recuperar de manera automática los elementos de la base de datos en el módulo de Productos
    
    * Revert "Se agregó el script para cargar de manera automática los campos de la base de datos (#27)"
    
    This reverts commit eb329518efbc929d6641b602457a1ff2658226c1.
    
    * Revert "Revert "Se agregó el script para cargar de manera automática los campos de la base de datos (#27)""
    
    This reverts commit 27e75feba11fdeb855e8c11b8e1672fafb798896.
    
    * Revert "Se agregó el script para cargar de manera automática los campos de la base de datos (#27)"
    
    This reverts commit eb329518efbc929d6641b602457a1ff2658226c1.
    
    * Revert "Conflicto de fusión resuelto"
    
    This reverts commit ea5446e1ed9e0b442c112f09ef3a58789b3196b1, reversing
    changes made to 33a7235ae50ee45c85565f506a6cf8e206bfd128.
    
    * Cambios y correcciones
    
    * correciones para carga masiva (csv)
    
    * Se agregó código parcial para leer apartados desde un csv y ponerlos en la base de datos
    
    * Actualización de proyecto
    
    * Funcionalidad de botones asignar apartados y pantallas de carga
    
    * Cambios en el script
    
    se quito Operador.correo, se cambio Producto.stock a NOT NULL, se agrego Ubicacion.prioridad, se cambio Control.asignado a Control.estado, se quito Operador_has_control.prioridad, cambios en los inserts con las nuevas modificaciones
    
    * cambios en tipos de dato
    
    se cambia de BOOLEAN a TINYINT(1) y se agrega el campo de activo en operador
    
    * Correcciones en la lógica para mostrar tablas y para el uso de la función init
    
    * Addición de un archivo modificado para mostrar las tablas
    
    * Modificación del boton de eliminar
    
    * Correción de error al momento de borrar operador
    
    * actualizacion
    
    * Actualizacion
    
    * Actualizacion
    
    * correcion en el archivo csv de ubicaciones
    
    * se modifico el script de la BD para funcionalidad con las ubicaciones de los productos
    
    * Se agrego la funcionalidad de leer las ubicaciones y funcionalidad para detectar que el csv subido sea el correcto
    
    * correcciones visuales
    
    * se agrego la funcionalidad de modificar
    
    * Se agrego la validacion del formulario
    
    Co-authored-by: Esteban Raymundo Olmedo Ramírez <43709368+EstebanOlmedo@users.noreply.github.com>
    Co-authored-by: EstebanOlmedo <esteban.olmedo.ramirez@gmail.com>
    Co-authored-by: EstebanOlmedo <esteban.olmedo.ramire@gmail.com>
    Co-authored-by: edoomm <edoomm8@gmail.com>
    Co-authored-by: KevinFrias <knightkevin99@gmail.com>

--- js/productos.js
+++ js/productos.js
@@ -1,3 +1,25 @@
+var sku = document.getElementById("inputSKU");
+var ubicacion = document.getElementById("inputUbicacion");
+var descripcion = document.getElementById("inputDescripcion");
+var stock = document.getElementById("inputStock");
+var medida = document.getElementById("inputMedida");
+var id_linea = document.getElementById("inputLinea");
+var generico = document.getElementById("inputGenerico");
+var skuMod = document.getElementById("modificarSKU");
+var ubicacionMod = document.getElementById("modificarUbicacion");
+var descripcionMod = document.getElementById("modificarDescripcion");
+var stockMod = document.getElementById("modificarStock");
+var medidaMod = document.getElementById("modificarMedida");
+var id_lineaMod = document.getElementById("modificarLinea");
+var genericoMod = document.getElementById("modificarGenerico");
+var skuVal;
+var ubicacionVal;
+var descripcionVal;
+var stockVal;
+var medidaVal;
+var id_lineaVal;
+var genericoVal;
+
 function filtrar() {
   // Declare variables
   var input, filter, table, tr, td, i, txtValue;
@@ -21,8 +43,35 @@ function filtrar() {
 
 function limpiarFormulario(id)
 {
-    document.getElementById(id).reset();
-
+  document.getElementById(id).reset();
+  sku.classList.remove('is-valid');
+  sku.classList.remove('is-invalid');
+  ubicacion.classList.remove('is-valid');
+  ubicacion.classList.remove('is-invalid');
+  descripcion.classList.remove('is-valid');
+  descripcion.classList.remove('is-invalid');
+  stock.classList.remove('is-valid');
+  stock.classList.remove('is-invalid');
+  medida.classList.remove('is-valid');
+  medida.classList.remove('is-invalid');
+  id_linea.classList.remove('is-valid');
+  id_linea.classList.remove('is-invalid');
+  generico.classList.remove('is-valid');
+  generico.classList.remove('is-invalid');
+  skuMod.classList.remove('is-valid');
+  skuMod.classList.remove('is-invalid');
+  ubicacionMod.classList.remove('is-valid');
+  ubicacionMod.classList.remove('is-invalid');
+  descripcionMod.classList.remove('is-valid');
+  descripcionMod.classList.remove('is-invalid');
+  stockMod.classList.remove('is-valid');
+  stockMod.classList.remove('is-invalid');
+  medidaMod.classList.remove('is-valid');
+  medidaMod.classList.remove('is-invalid');
+  id_lineaMod.classList.remove('is-valid');
+  id_lineaMod.classList.remove('is-invalid');
+  genericoMod.classList.remove('is-valid');
+  genericoMod.classList.remove('is-invalid');
 }
 
 function iniciarCargaArchivo(tipo)
@@ -37,20 +86,334 @@ function iniciarCargaArchivo(tipo)
     document.getElementById("btn-archivoUbicacion").disabled=true;
     document.getElementById("targetLayerUbicacion").innerHTML = document.getElementById("modalInicioUbicacion").innerHTML;
   }
-  
 }
 
-function validarFormulario()
+function validarCamposLlenos(tipo)
 {
-  console.log("hola");
-  let sku = document.getElementById("inputSKU").value;
-  let ubicacion = document.getElementById("inputUbicacion").value;
-  let descripcion = document.getElementById("inputDescripcion").value;
-  let stock = document.getElementById("inputStock").value;
-  let medida = document.getElementById("inputMedida").value;
-  let id_linea = document.getElementById("inputLinea").value;
-  let generico = document.getElementById("inputGenerico").value;
-  
+  var flag = true;
+  if(tipo == 1)
+  {
+    if(skuVal === "")
+    {
+      sku.classList.add('is-invalid');
+      document.getElementById('invalid_sku').getElementsByTagName('p')[0].innerHTML = "Ingresa el SKU";
+      flag = false;
+    }
+    if(ubicacionVal === "")
+    {
+      ubicacion.classList.add('is-invalid');
+      document.getElementById('invalid_ubicacion').getElementsByTagName('p')[0].innerHTML = "Ingresa la ubicación";
+      flag = false;
+    }
+    if(stockVal === "")
+    {
+      stock.classList.add('is-invalid');
+      document.getElementById('invalid_stock').getElementsByTagName('p')[0].innerHTML = "Ingresa el stock";
+      flag = false;
+    }
+    if(descripcionVal === "")
+    {
+      descripcion.classList.add('is-invalid');
+      document.getElementById('invalid_descripcion').getElementsByTagName('p')[0].innerHTML = "Ingresa la descripción";
+      flag = false;
+    }
+    if(medidaVal === "")
+    {
+      medida.classList.add('is-invalid');
+      document.getElementById('invalid_medida').getElementsByTagName('p')[0].innerHTML = "Ingresa la medida";
+      flag = false;
+    }
+    if(id_lineaVal === "")
+    {
+      id_linea.classList.add('is-invalid');
+      document.getElementById('invalid_linea').getElementsByTagName('p')[0].innerHTML = "Ingresa el ID de linea";
+      flag = false;
+    }
+    if(genericoVal === "")
+    {
+      generico.classList.add('is-invalid');
+      document.getElementById('invalid_generico').getElementsByTagName('p')[0].innerHTML = "Ingresa el generico";
+      flag = false;
+    }
+  }
+  else 
+  {
+    if(skuVal === "")
+    {
+      skuMod.classList.add('is-invalid');
+      document.getElementById('invalid_skuMod').getElementsByTagName('p')[0].innerHTML = "Ingresa el SKU";
+      flag = false;
+    }
+    if(ubicacionVal === "")
+    {
+      ubicacionMod.classList.add('is-invalid');
+      document.getElementById('invalid_ubicacionMod').getElementsByTagName('p')[0].innerHTML = "Ingresa la ubicación";
+      flag = false;
+    }
+    if(descripcionVal === "")
+    {
+      descripcionMod.classList.add('is-invalid');
+      document.getElementById('invalid_descripcionMod').getElementsByTagName('p')[0].innerHTML = "Ingresa la descripción";
+      flag = false;
+    }
+    if(stockVal === "")
+    {
+      stockMod.classList.add('is-invalid');
+      document.getElementById('invalid_stockMod').getElementsByTagName('p')[0].innerHTML = "Ingresa el stock";
+      flag = false;
+    }
+    if(medidaVal === "")
+    {
+      medidaMod.classList.add('is-invalid');
+      document.getElementById('invalid_medidaMod').getElementsByTagName('p')[0].innerHTML = "Ingresa la medida";
+      flag = false;
+    }
+    if(id_lineaVal === "")
+    {
+      id_lineaMod.classList.add('is-invalid');
+      document.getElementById('invalid_lineaMod').getElementsByTagName('p')[0].innerHTML = "Ingresa el ID de linea";
+      flag = false;
+    }
+    if(genericoVal === "")
+    {
+      genericoMod.classList.add('is-invalid');
+      document.getElementById('invalid_genericoMod').getElementsByTagName('p')[0].innerHTML = "Ingresa el generico";
+      flag = false;
+    }
+  }
+  return flag;
+}
+
+function validarSKU(idInput,idError)
+{
+  if(Number.isInteger(parseFloat(skuVal)))
+  {
+    if(parseFloat(skuVal) > 0)
+    {
+      idInput.classList.add('is-valid');
+      return true;
+    }
+    else
+    {
+      idInput.classList.add('is-invalid');
+      document.getElementById(idError).getElementsByTagName('p')[0].innerHTML = "El SKU debe ser un número entero positivo";
+      return false; 
+    }
+  }
+  else
+  {
+    idInput.classList.add('is-invalid');
+    document.getElementById(idError).getElementsByTagName('p')[0].innerHTML = "El SKU debe ser un número entero";
+    return false;
+  }
+}
+
+function validarUbicacion(idInput,idError)
+{
+  let patron = /[A-Z]\.[0-9][0-9]\.0[1-4]\.0[2-8]$/;
+  if(patron.test(ubicacionVal))
+  {
+    idInput.classList.add('is-valid');
+    return true;
+  }
+  else
+  {
+    idInput.classList.add('is-invalid');
+    document.getElementById(idError).getElementsByTagName('p')[0].innerHTML = "Verifica el formato de la ubicación";
+    return false;
+  }
+}
+
+function validarDescripcion(idInput,idError)
+{
+  if(descripcionVal.length > 0 && descripcionVal.length <=100)
+  {
+    idInput.classList.add('is-valid');
+    return true;
+  }
+  else
+  {
+    idInput.classList.add('is-invalid');
+    document.getElementById(idError).getElementsByTagName('p')[0].innerHTML = "La longitud mínima de la descripción es 1 y la mayor es 100";
+    return false;
+  }
+}
+
+function validarStock(idInput,idError)
+{
+  if(Number.isInteger(parseFloat(stockVal)))
+  {
+    if(parseFloat(stockVal) > 0)
+    {
+      idInput.classList.add('is-valid');
+      return true;
+    }
+    else
+    {
+      idInput.classList.add('is-invalid');
+      document.getElementById(idError).getElementsByTagName('p')[0].innerHTML = "El stock debe ser un número entero positivo";
+      return false; 
+    }
+  }
+  else
+  {
+    idInput.classList.add('is-invalid');
+    document.getElementById(idError).getElementsByTagName('p')[0].innerHTML = "El stock debe ser un número entero";
+    return false;
+  }
+}
+
+function validarUnidadMedida(idInput,idError)
+{
+  if(Number.isInteger(parseFloat(medidaVal)))
+  {
+    if(parseFloat(medidaVal) > 0)
+    {
+      idInput.classList.add('is-valid');
+      return true;
+    }
+    else
+    {
+      idInput.classList.add('is-invalid');
+      document.getElementById(idError).getElementsByTagName('p')[0].innerHTML = "La unidad de medida debe ser un número entero positivo";
+      return false; 
+    }
+  }
+  else
+  {
+    idInput.classList.add('is-invalid');
+    document.getElementById(idError).getElementsByTagName('p')[0].innerHTML = "La unidad de medida debe ser un número entero";
+    return false;
+  }
+}
+
+function validarIDLinea(idInput,idError)
+{
+  let patron = /[A-Z][A-Z][A-Z]$/;
+  if(patron.test(id_lineaVal))
+  {
+    idInput.classList.add('is-valid');
+    return true;
+  }
+  else
+  {
+    idInput.classList.add('is-invalid');
+    document.getElementById(idError).getElementsByTagName('p')[0].innerHTML = "Verifica el formato del ID de Linea";
+    return false;
+  }
+}
+
+function validarGenerico(idInput,idError)
+{
+  let patron = /[A-Z]$/;
+  if(patron.test(genericoVal))
+  {
+    idInput.classList.add('is-valid');
+    return true;
+  }
+  else
+  {
+    idInput.classList.add('is-invalid');
+    document.getElementById(idError).getElementsByTagName('p')[0].innerHTML = "Verifica el formato del genérico";
+    return false;
+  }
+}
+
+function validarFormulario(tipo)
+{
+  var valido = true;
+  if(tipo == 1)
+  {
+    skuVal = sku.value;
+    ubicacionVal = ubicacion.value;
+    descripcionVal = descripcion.value;
+    stockVal = stock.value;
+    medidaVal = medida.value;
+    id_lineaVal = id_linea.value;
+    genericoVal = generico.value; 
+    if(!validarCamposLlenos(tipo))
+    {
+      valido = false;
+    }
+    if(!validarSKU(sku,"invalid_sku"));
+    {
+      valido = false;
+    }
+    if(!validarUbicacion(ubicacion,"invalid_ubicacion"))
+    {
+      valido = false;
+    }
+    if(!validarDescripcion(descripcion,"invalid_descripcion"))
+    {
+      valido = false;
+    }
+    if(!validarStock(stock,"invalid_stock"))
+    {
+      valido = false;
+    }
+    if(!validarUnidadMedida(medida,"invalid_medida"))
+    {
+      valido = false;
+    }
+    if(!validarIDLinea(id_linea,"invalid_linea"))
+    {
+      valido = false;
+    }
+    if(!validarGenerico(generico,"invalid_generico"))
+    {
+      valido = false;
+    }
+    if(valido)
+    {
+      console.log("Formulario correcto")
+    }
+  }
+  else if(tipo == 2)
+  {
+    skuVal = skuMod.value;
+    ubicacionVal = ubicacionMod.value;
+    descripcionVal = descripcionMod.value;
+    stockVal = stockMod.value;
+    medidaVal = medidaMod.value;
+    id_lineaVal = id_lineaMod.value;
+    genericoVal = genericoMod.value;
+    if(!validarCamposLlenos(tipo))
+    {
+      return false;
+    }
+    if(!validarSKU(skuMod,"invalid_skuMod"));
+    {
+      valido = false;
+    }
+    if(!validarUbicacion(ubicacionMod,"invalid_ubicacionMod"))
+    {
+      valido = false;
+    }
+    if(!validarDescripcion(descripcionMod,"invalid_descripcionMod"))
+    {
+      valido = false;
+    }
+    if(!validarStock(stockMod,"invalid_stockMod"))
+    {
+      valido = false;
+    }
+    if(!validarUnidadMedida(medidaMod,"invalid_medidaMod"))
+    {
+      valido = false;
+    }
+    if(!validarIDLinea(id_lineaMod,"invalid_lineaMod"))
+    {
+      valido = false;
+    }
+    if(!validarGenerico(genericoMod,"invalid_genericoMod"))
+    {
+      valido = false;
+    }
+    if(valido)
+    {
+      console.log("Formulario correcto")
+    }
+  }
 }
 
 function removeDragData(event){
@@ -96,6 +459,7 @@ function sendFile(ubicacion,tipo){
               else
                   alert("Verifica que el archivo sea de las ubicaciones");
           }
+          alert("Se subio correctamete el archivo");
           location.reload();
       }
   };
@@ -105,15 +469,16 @@ function sendFile(ubicacion,tipo){
 }
 
 function dropHandler(event){
+  
   event.preventDefault();
   if($('#subirArchivoInventario').is(':visible'))
   {
-    console.log("Entra");
     document.getElementById("btn-archivoInventario").disabled = false;
   }
   else
     document.getElementById("btn-archivoUbicacion").disabled = false;
   var nombreDeArchivos = [];
+ 
   if(event.dataTransfer.items)
   {
       for(var i = 0; i < event.dataTransfer.items.length; i++){
@@ -125,10 +490,10 @@ function dropHandler(event){
       }
       if($('#subirArchivoInventario').is(':visible'))
       {
-        
+        console.log(nombreDeArchivos);        
         for(var i = 0; i < nombreDeArchivos.length; i++)
         {
-          document.getElementById("mostrarNombreArchivoInventario").textContent += nombreDeArchivos[i] + " ";
+          document.getElementById("mostrarNombreArchivoInventario").textContent = "Se cargó correctamente el archivo: \n" + nombreDeArchivos[i] + " ";
         }
         document.getElementById("targetLayerInventario").innerHTML = document.getElementById("archivoCargadoInventario").innerHTML;
         removeDragData(event);
@@ -138,7 +503,7 @@ function dropHandler(event){
         
         for(var i = 0; i < nombreDeArchivos.length; i++)
         {
-            document.getElementById("mostrarNombreArchivoUbicacion").textContent += nombreDeArchivos[i] + " ";
+            document.getElementById("mostrarNombreArchivoUbicacion").textContent = "Se cargó correctamente el archivo: \n" +nombreDeArchivos[i] + " ";
         }
         document.getElementById("targetLayerUbicacion").innerHTML = document.getElementById("archivoCargadoUbicacion").innerHTML;
         removeDragData(event);
