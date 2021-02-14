4888b2f26f33be18d863a8546a60b3faf8731feb
brielgah <gabrielgh@live.com.mx>
2021-02-13 21:29:45 -0600

    Se agrego la opcion de asignar varias ubicaciones a un producto

--- php/productos/productosIngresar.php
+++ php/productos/productosIngresar.php
@@ -6,12 +6,10 @@
     $descripcion = $_POST['descripcion'];
     $stock = $_POST['stock'];
     $unidad_medida = $_POST['medida']; 
-    $ubicacion = $_POST['ubicacion'];
+    $ubicacion = "SIN ASIGNAR";
     $flag = FALSE;
     $link = open_database();
-    echo $sku;
     $sql_query = "INSERT INTO Producto (sku, id_linea, generico, unidad_medida, descripcion,stock,ubicacion) VALUES('".$sku."','".$id_linea."','".$generico."','".$unidad_medida."','".$descripcion."','".$stock."','".$ubicacion."')";
-    echo $sql_query;
     $result = $link->query($sql_query);
     $link->close();
 
