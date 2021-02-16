3bc60164090d80b9a351da127b59cfe5b7406f40
Esteban Raymundo Olmedo Ramírez <43709368+EstebanOlmedo@users.noreply.github.com>
2021-02-15 22:50:18 -0600

    Se cambió la forma en la que se genera el archivo CSV (#59)
    * Se agregó el popup para subir el archivo CSV
    
    * Se añadió funcionalidad para mostrar los datos de operador una vez que se da click sobre una fila de la tabla
    
    * Script para poder subir el archivo .csv
    
    * Correcciones en el script para cargar archivo csv
    
    * Se corrigió el script para subir archivos csv en el apartado de productos
    
    * Correción en la lógica para subir archivo y correcciones en página de apartados y productos
    
    * Correcciones en la página de operadores y la página de productos
    
    * Se agregó el script para recuperar de manera automática los elementos de la base de datos en el módulo de Productos
    
    * Revert "Se agregó el script para cargar de manera automática los campos de la base de datos (#27)"
    
    This reverts commit eb329518efbc929d6641b602457a1ff2658226c1.
    
    * Revert "Revert "Se agregó el script para cargar de manera automática los campos de la base de datos (#27)""
    
    This reverts commit 27e75feba11fdeb855e8c11b8e1672fafb798896.
    
    * Revert "Se agregó el script para cargar de manera automática los campos de la base de datos (#27)"
    
    This reverts commit eb329518efbc929d6641b602457a1ff2658226c1.
    
    * Revert "Conflicto de fusión resuelto"
    
    This reverts commit ea5446e1ed9e0b442c112f09ef3a58789b3196b1, reversing
    changes made to 33a7235ae50ee45c85565f506a6cf8e206bfd128.
    
    * Cambios y correcciones
    
    * correciones para carga masiva (csv)
    
    * Se agregó código parcial para leer apartados desde un csv y ponerlos en la base de datos
    
    * Actualización de proyecto
    
    * Funcionalidad de botones asignar apartados y pantallas de carga
    
    * Cambios en el script
    
    se quito Operador.correo, se cambio Producto.stock a NOT NULL, se agrego Ubicacion.prioridad, se cambio Control.asignado a Control.estado, se quito Operador_has_control.prioridad, cambios en los inserts con las nuevas modificaciones
    
    * cambios en tipos de dato
    
    se cambia de BOOLEAN a TINYINT(1) y se agrega el campo de activo en operador
    
    * Correcciones en la lógica para mostrar tablas y para el uso de la función init
    
    * Addición de un archivo modificado para mostrar las tablas
    
    * Modificación del boton de eliminar
    
    * Correción de error al momento de borrar operador
    
    * actualizacion
    
    * Actualizacion
    
    * Actualizacion
    
    * Corrección en la lógica de la asignación automática
    
    * Corrección de errores para la asignación automática de apartados
    
    * Solucionado conflicto de fusión
    
    * Se agregó la funcionalidad para generar el archivo csv
    
    * Corrección para la formación del archivo CSV
    
    Co-authored-by: EstebanOlmedo <esteban.olmedo.ramire@gmail.com>
    Co-authored-by: edoomm <edoomm8@gmail.com>
    Co-authored-by: KevinFrias <knightkevin99@gmail.com>
    Co-authored-by: brielgah <gabrielgh@live.com.mx>
    Co-authored-by: Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>

--- js/apartados.js
+++ js/apartados.js
@@ -239,18 +239,16 @@ async function mostrarSinAsignar(){
     );
 }
 function download(filename, text) {
-    var pom = document.createElement('a');
-    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    pom.setAttribute('download', filename);
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
 
-    if (document.createEvent) {
-        var event = document.createEvent('MouseEvents');
-        event.initEvent('click', true, true);
-        pom.dispatchEvent(event);
-    }
-    else {
-        pom.click();
-    }
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
 }
 function generarReporte(){
     $.post("../php/apartados/generarReporte.php",{},
